1624a72f65e11d9c1b6126f6adcb2301abd8f731
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-07 17:46:38 +0300

    Create MyBalance component

---
+++ src/components/MyBalance/MyBalance.jsx
@@ -0,0 +1,18 @@
+export default function MyBalance() {
+  return (
+    <header>
+      <div className="balance">
+        <h1>My balance</h1>
+        <p>$921.48</p>
+      </div>
+      <div className="logo">
+        <svg width="72" height="48" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fillRule="evenodd">
+            <circle fill="#382314" cx="48" cy="24" r="24" />
+            <circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23" />
+          </g>
+        </svg>
+      </div>
+    </header>
+  );
+}
