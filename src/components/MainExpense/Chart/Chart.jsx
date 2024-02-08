4ae199b4c2138db5176dfdc1dee9e0cc0ad411a0
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-08 16:07:37 +0300

    Map over data from JSON

--- src/components/MainExpense/Chart/Chart.jsx
+++ src/components/MainExpense/Chart/Chart.jsx
@@ -1,17 +1,19 @@
 import data from "../../../../data.json";
-import './Chart.css';
+import "./Chart.css";
 
 export default function Chart() {
   return (
     <div className="chart">
       <div className="bars">
-        <div className="bar bar-1" data-tooltip={data[0].amount}></div>
-        <div className="bar bar-2" data-tooltip={data[1].amount}></div>
-        <div className="bar bar-3"></div>
-        <div className="bar bar-4"></div>
-        <div className="bar bar-5"></div>
-        <div className="bar bar-6"></div>
-        <div className="bar bar-7"></div>
+        {data.map((chart, index) => {
+          return (
+            <div
+              key={chart.day}
+              className={`bar bar-${index + 1}`}
+              data-tooltip={chart.amount}
+            ></div>
+          );
+        })}
       </div>
       <div className="days">
         {data.map((chart) => {
