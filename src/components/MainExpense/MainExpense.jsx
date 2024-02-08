1224444c8367993f895d3badbd3ea8f09de24a91
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-08 16:00:06 +0300

    Add Chart in components tree

--- src/components/MainExpense/MainExpense.jsx
+++ src/components/MainExpense/MainExpense.jsx
@@ -1,29 +1,14 @@
 import TotalExpense from "./TotalExpense/TotalExpense";
+import Chart from "./Chart/chart";
 import "./MainExpense.css";
-import data from '../../../data.json';
+
 
 export default function MainExpense() {
   return (
     <main>
       <section className="spending">
         <h2>Spending - Last 7 days</h2>
-        <div className="chart">
-          <div className="bars">
-            <div className="bar bar-1" data-tooltip={data[0].amount}></div>
-            <div className="bar bar-2" data-tooltip={data[1].amount}></div>
-            <div className="bar bar-3"></div>
-            <div className="bar bar-4"></div>
-            <div className="bar bar-5"></div>
-            <div className="bar bar-6"></div>
-            <div className="bar bar-7"></div>
-          </div>
-          <div className="days">
-            {data.map((chart)=>{
-              return <p key={chart.day}>{chart.day}</p>
-            })}
-          </div>
-        </div>
-        {/* mon tue wed thu fri sat sun */}
+        <Chart/>
         <TotalExpense />
       </section>
     </main>
