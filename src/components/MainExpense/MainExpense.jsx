534331fbf836098cd52902fd804904f567b8feea
amrmabdelazeem <amr.abdelazeem22@gmail.com>
2024-02-08 13:23:11 +0300

    Add chart bars and days

--- src/components/MainExpense/MainExpense.jsx
+++ src/components/MainExpense/MainExpense.jsx
@@ -1,12 +1,30 @@
 import TotalExpense from "./TotalExpense/TotalExpense";
-import './MainExpense.css'
+import "./MainExpense.css";
+import data from '../../../data.json';
+
 export default function MainExpense() {
   return (
     <main>
-    <section>
-      <h2>Spending - Last 7 days</h2>
-      mon tue wed thu fri sat sun
-      <TotalExpense />
+      <section className="spending">
+        <h2>Spending - Last 7 days</h2>
+        <div className="chart">
+          <div className="bars">
+            <div className="bar bar-1"></div>
+            <div className="bar bar-2"></div>
+            <div className="bar bar-3"></div>
+            <div className="bar bar-4"></div>
+            <div className="bar bar-5"></div>
+            <div className="bar bar-6"></div>
+            <div className="bar bar-7"></div>
+          </div>
+          <div className="days">
+            {data.map((chart)=>{
+              return <p key={chart.day}>{chart.day}</p>
+            })}
+          </div>
+        </div>
+        {/* mon tue wed thu fri sat sun */}
+        <TotalExpense />
       </section>
     </main>
   );
